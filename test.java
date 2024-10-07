2f44f3a895c0b4e6022fbbd3beffa698d74dc5cd
Pranavi Patil <123371662+pranavipatil@users.noreply.github.com>
2024-10-07 10:15:20 +0530

    Create test.java

---
+++ test.java
@@ -0,0 +1,7 @@
+class test
+  {
+    public static void main(String arg[])
+    {
+        System.out.println("This is a program from my github account and executing it using google cloud console");
+    }
+  }
