887b7ee99d2951e30c6fda3f08288ccde7854dd1
Pranavi Patil <123371662+pranavipatil@users.noreply.github.com>
2024-10-07 10:19:45 +0530

    Update test.java

--- test.java
+++ test.java
@@ -2,6 +2,6 @@ class test
   {
     public static void main(String arg[])
     {
-        System.out.println("This is a program from my github account and executing it using google cloud console");
+        System.out.println("\n This is a program from my github account and executing it using google cloud console \n");
     }
   }
